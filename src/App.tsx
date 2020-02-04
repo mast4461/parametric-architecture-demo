27c9250f092cec8980f1a3ca6b8abc8dc19fb518
Martin Stålberg <mast4461@gmail.com>
2020-02-04 13:53:44 +0100

    refactor App to class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="https://datatjej.se/wp-content/uploads/2018/11/logo_datatjej_high_konverterad-online.png" className="App-logo" alt="logo" />
-        <h1>Hello DataTjej!</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://datatjej.se/wp-content/uploads/2018/11/logo_datatjej_high_konverterad-online.png" className="App-logo" alt="logo" />
+          <h1>Hello DataTjej!</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
