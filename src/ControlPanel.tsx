b4414edf9cac3a4eecf7b87324b1bb490a2950a8
Martin Stålberg <mast4461@gmail.com>
2020-02-07 10:35:57 +0100

    6. Add controls object

--- src/ControlPanel.tsx
+++ src/ControlPanel.tsx
@@ -1,4 +1,33 @@
-import React from "react";
+import React, { InputHTMLAttributes } from "react";
+
+type InputType = "range" | "color";
+const t = (obj: InputHTMLAttributes<HTMLInputElement> & {type: InputType}) => obj;
+
+const controls = {
+  width: t({
+    type: "range",
+    min: 1,
+    max: 10,
+    step: 0.01,
+    defaultValue: 1,
+  }),
+  height: t({
+    type: "range",
+    min: 1,
+    max: 10,
+    step: 0.01,
+    defaultValue: 1,
+  }),
+  color: t({
+    type: "color",
+    min: 1,
+    max: 10,
+    step: 0.01,
+    defaultValue: "#ed2151",
+  }),
+};
+
+type ControlName = keyof typeof controls;
 
 export default class ControlPanel extends React.Component {
   render() {
